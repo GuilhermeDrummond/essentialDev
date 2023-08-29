b38e5fb7a380e84def20df37aceb483a6ab5383e
Guilherme Drummond <g_drummond@hotmail.com>
2023-08-29 14:33:04 -0300

    Essential Dev

---
+++ scripts.js
@@ -0,0 +1,8 @@
+let clicar = document.getElementById("button");
+let email = document.getElementById("E-mail");  
+let password = document.getElementById("senha");  
+
+function entrar() {
+    alert('BONS ESTUDOS')
+    window.location.href = "index.html";
+}
